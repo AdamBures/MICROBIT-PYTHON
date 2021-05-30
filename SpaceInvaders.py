436e7f9a0299c7e7f5d7a8661ed735b1dfdff293
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-05-30 23:47:36 +0200

    Create SpaceInvaders.py

---
+++ SpaceInvaders.py
@@ -0,0 +1,77 @@
+def second_level():
+    global enemy_strela
+    first_level()
+    enemy_strela = game.create_sprite(nepritel1.get(LedSpriteProperty.X),
+        nepritel1.get(LedSpriteProperty.Y))
+    enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 80)
+    if lod.is_touching_edge():
+        for index in range(4):
+            enemy_strela.change(LedSpriteProperty.Y, 1)
+            basic.pause(200)
+        enemy_strela.delete()
+
+def on_button_pressed_a():
+    lod.move(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def first_level():
+    global lod, nepritel1, nepritel2, nepritel3, nepritel4, nepritel5
+    lod = game.create_sprite(2, 4)
+    basic.pause(200)
+    nepritel1 = game.create_sprite(0, 0)
+    basic.pause(200)
+    nepritel2 = game.create_sprite(1, 1)
+    basic.pause(200)
+    nepritel3 = game.create_sprite(2, 0)
+    basic.pause(200)
+    nepritel4 = game.create_sprite(3, 1)
+    basic.pause(200)
+    nepritel5 = game.create_sprite(4, 0)
+
+def on_button_pressed_ab():
+    global strela, hitbox, hitbox2
+    strela = game.create_sprite(lod.get(LedSpriteProperty.X), lod.get(LedSpriteProperty.Y))
+    hitbox = game.create_sprite(1, 0)
+    hitbox2 = game.create_sprite(3, 0)
+    strela.change(LedSpriteProperty.BRIGHTNESS, 80)
+    hitbox.set(LedSpriteProperty.BRIGHTNESS, 0)
+    hitbox2.set(LedSpriteProperty.BRIGHTNESS, 0)
+    for index2 in range(4):
+        strela.change(LedSpriteProperty.Y, -1)
+        basic.pause(200)
+    if strela.is_touching(nepritel1):
+        nepritel1.delete()
+    elif strela.is_touching(hitbox):
+        nepritel2.delete()
+        hitbox.delete()
+    elif strela.is_touching(nepritel3):
+        nepritel3.delete()
+    elif strela.is_touching(hitbox2):
+        nepritel4.delete()
+        hitbox2.delete()
+    elif strela.is_touching(nepritel5):
+        nepritel5.delete()
+    strela.delete()
+    if nepritel1.is_deleted() and nepritel2.is_deleted() and nepritel3.is_deleted() and nepritel4.is_deleted() and nepritel5.is_deleted():
+        lod.delete()
+        basic.show_string("LEVEL 1 COMPLETED!")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    lod.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+hitbox2: game.LedSprite = None
+hitbox: game.LedSprite = None
+strela: game.LedSprite = None
+nepritel5: game.LedSprite = None
+nepritel4: game.LedSprite = None
+nepritel3: game.LedSprite = None
+nepritel2: game.LedSprite = None
+lod: game.LedSprite = None
+nepritel1: game.LedSprite = None
+enemy_strela: game.LedSprite = None
+nepritel6 = None
+nepritel: List[number] = []
+enemy_strela = None
+second_level()

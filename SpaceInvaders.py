1321b8b5f1067d50828867c57bcfec6d36d5e691
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-06-01 22:51:21 +0200

    Update SpaceInvaders.py

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,14 +1,35 @@
+"""
+TODO:
+    WORKING SECOND LEVEL SHOOTING
+    WORKING THIRD LEVEL
+    LEVEL CONSTANT
+    SCORE CONSTANT 
+    LOADING MENU 
+    TEXT
+"""
+
+
 def second_level():
-    global enemy_strela
-    first_level()
-    enemy_strela = game.create_sprite(nepritel1.get(LedSpriteProperty.X),
-        nepritel1.get(LedSpriteProperty.Y))
-    enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 80)
-    if lod.is_touching_edge():
-        for index in range(4):
-            enemy_strela.change(LedSpriteProperty.Y, 1)
-            basic.pause(200)
-        enemy_strela.delete()
+    global enemy_strela, lod
+    lod = game.create_sprite(2, 4)    
+    # enemy_strela = game.create_sprite(randint(0, 4), 0)
+    # enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 150)
+    # enemy_strela.turn(Direction.RIGHT, 90)
+    # for index in range(4):
+    #     enemy_strela.move(1)
+    #     basic.pause(500)
+    # enemy_strela.delete()
+    # if enemy_strela.is_touching_edge():
+    #     game.game_over()
+
+def on_forever():
+    enemy_strela = game.create_sprite(randint(0, 4), 0)
+    enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 150)
+    enemy_strela.turn(Direction.RIGHT, 90)
+    for index in range(4):
+        enemy_strela.move(1)
+        basic.pause(500)
+    enemy_strela.delete()
 
 def on_button_pressed_a():
     lod.move(-1)
@@ -54,7 +75,8 @@ def on_button_pressed_ab():
     strela.delete()
     if nepritel1.is_deleted() and nepritel2.is_deleted() and nepritel3.is_deleted() and nepritel4.is_deleted() and nepritel5.is_deleted():
         lod.delete()
-        basic.show_string("LEVEL 1 COMPLETED!")
+        # basic.show_string("LEVEL 1 COMPLETED!")
+        basic.forever(on_forever)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
@@ -68,10 +90,17 @@ nepritel5: game.LedSprite = None
 nepritel4: game.LedSprite = None
 nepritel3: game.LedSprite = None
 nepritel2: game.LedSprite = None
-lod: game.LedSprite = None
 nepritel1: game.LedSprite = None
+lod: game.LedSprite = None
 enemy_strela: game.LedSprite = None
-nepritel6 = None
-nepritel: List[number] = []
-enemy_strela = None
-second_level()
+lod = game.create_sprite(2, 4)
+basic.pause(200)
+nepritel1 = game.create_sprite(0, 0)
+basic.pause(200)
+nepritel2 = game.create_sprite(1, 1)
+basic.pause(200)
+nepritel3 = game.create_sprite(2, 0)
+basic.pause(200)
+nepritel4 = game.create_sprite(3, 1)
+basic.pause(200)
+nepritel5 = game.create_sprite(4, 0)

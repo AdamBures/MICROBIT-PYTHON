9ca9233c04bb7213c27095d92248edfdadaa7ac7
AdamBures <47189693+AdamBures@users.noreply.github.com>
2021-06-08 18:41:46 +0200

    Update SpaceInvaders.py

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -3,8 +3,10 @@
 # WORKING THIRD LEVEL
 # LEVEL CONSTANT
 # SCORE CONSTANT
+# ADD TIME CONSTANT
 # LOADING MENU
 # TEXT
+# PEP8 CONVENTION
 def second_level():
     first_level()
 
@@ -27,7 +29,7 @@ def first_level():
     nepritel5 = game.create_sprite(4, 0)
 
 def on_button_pressed_ab():
-    global strela, hitbox, hitbox2, level
+    global strela, hitbox, hitbox2, level, score
     strela = game.create_sprite(lod.get(LedSpriteProperty.X), lod.get(LedSpriteProperty.Y))
     hitbox = game.create_sprite(1, 0)
     hitbox2 = game.create_sprite(3, 0)
@@ -39,20 +41,25 @@ def on_button_pressed_ab():
         basic.pause(200)
     if strela.is_touching(nepritel1):
         nepritel1.delete()
+        score += 100
     elif strela.is_touching(hitbox):
         nepritel2.delete()
         hitbox.delete()
+        score += 100
     elif strela.is_touching(nepritel3):
         nepritel3.delete()
+        score += 100
     elif strela.is_touching(hitbox2):
         nepritel4.delete()
         hitbox2.delete()
+        score += 100
     elif strela.is_touching(nepritel5):
         nepritel5.delete()
+        score += 100
     strela.delete()
     if nepritel1.is_deleted() and nepritel2.is_deleted() and nepritel3.is_deleted() and nepritel4.is_deleted() and nepritel5.is_deleted():
         lod.delete()
-        basic.show_string("" + str((level)))
+        basic.show_string("" + str(level))
         level = level + 1
         second_level()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
@@ -61,6 +68,7 @@ def on_button_pressed_b():
     lod.move(1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+score = 0
 enemy_strela: game.LedSprite = None
 hitbox2: game.LedSprite = None
 hitbox: game.LedSprite = None
@@ -85,16 +93,20 @@ basic.pause(200)
 nepritel4 = game.create_sprite(3, 1)
 basic.pause(200)
 nepritel5 = game.create_sprite(4, 0)
+TIME = 500
+SPEED = 0.5
 
 def on_forever():
     global enemy_strela
-    if level == 2:
-        enemy_strela = game.create_sprite(randint(0, 4), 0)
-        enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 150)
-        basic.pause(100)
-        enemy_strela.turn(Direction.RIGHT, 90)
-        for index2 in range(4):
-            enemy_strela.move(1)
-            basic.pause(500)
-        enemy_strela.delete()
+    enemy_strela = game.create_sprite(randint(0, 4), 0)
+    enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 150)
+    basic.pause(100)
+    enemy_strela.turn(Direction.RIGHT, 90)
+    for index2 in range(4):
+        enemy_strela.move(1)
+        basic.pause(TIME//SPEED)
+    if enemy_strela.is_touching(lod):
+        basic.show_string("GAME OVER!")
+        basic.show_string(str(score))
+    enemy_strela.delete()
 basic.forever(on_forever)

216241af9b02da035185a49027c72b6eee627822
AdamBures <47189693+adambures@users.noreply.github.com>
2021-06-09 22:58:19 +0200

    Finish TODO, ADDED TIME CONSTANT, Loading menu, final text, resetting level and ending game, THE GAME WORKS!

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,125 +1,119 @@
-"""
-TODO:
- ADD TIME CONSTANT
- LOADING MENU
- TEXT
- Resetting level and ending game
-"""
-
-
-def second_level():
-    first_level()
-
-
 def on_button_pressed_a():
-    lod.move(-1)
-
-
+    ship.move(-1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 
 def first_level():
-    global lod, nepritel1, nepritel2, nepritel3, nepritel4, nepritel5
-    lod = game.create_sprite(2, 4)
+    global ship, enemy1, enemy2, enemy3, enemy4, enemy5
+    ship = game.create_sprite(2, 4)
     basic.pause(200)
-    nepritel1 = game.create_sprite(0, 0)
+    enemy1 = game.create_sprite(0, 0)
     basic.pause(200)
-    nepritel2 = game.create_sprite(1, 1)
+    enemy2 = game.create_sprite(1, 1)
     basic.pause(200)
-    nepritel3 = game.create_sprite(2, 0)
+    enemy3 = game.create_sprite(2, 0)
     basic.pause(200)
-    nepritel4 = game.create_sprite(3, 1)
+    enemy4 = game.create_sprite(3, 1)
     basic.pause(200)
-    nepritel5 = game.create_sprite(4, 0)
+    enemy5 = game.create_sprite(4, 0)
 
 
 def on_button_pressed_ab():
-    global strela, hitbox, hitbox2, level, score
-    strela = game.create_sprite(lod.get(LedSpriteProperty.X), lod.get(LedSpriteProperty.Y))
+    global shoot, hitbox, hitbox2, SCORE, LEVEL, SPEED
+    shoot = game.create_sprite(ship.get(LedSpriteProperty.X), ship.get(LedSpriteProperty.Y))
     hitbox = game.create_sprite(1, 0)
     hitbox2 = game.create_sprite(3, 0)
-    strela.change(LedSpriteProperty.BRIGHTNESS, 80)
+    shoot.change(LedSpriteProperty.BRIGHTNESS, 80)
     hitbox.set(LedSpriteProperty.BRIGHTNESS, 0)
     hitbox2.set(LedSpriteProperty.BRIGHTNESS, 0)
     for index in range(4):
-        strela.change(LedSpriteProperty.Y, -1)
+        shoot.change(LedSpriteProperty.Y, -1)
         basic.pause(200)
-    if strela.is_touching(nepritel1):
-        nepritel1.delete()
-        score += 100
-    elif strela.is_touching(hitbox):
-        nepritel2.delete()
+    if shoot.is_touching(enemy1):
+        enemy1.delete()
+        SCORE += 100
+    elif shoot.is_touching(hitbox):
+        enemy2.delete()
         hitbox.delete()
-        score += 100
-    elif strela.is_touching(nepritel3):
-        nepritel3.delete()
-        score += 100
-    elif strela.is_touching(hitbox2):
-        nepritel4.delete()
+        SCORE += 100
+    elif shoot.is_touching(enemy3):
+        enemy3.delete()
+        SCORE += 100
+    elif shoot.is_touching(hitbox2):
+        enemy4.delete()
         hitbox2.delete()
-        score += 100
-    elif strela.is_touching(nepritel5):
-        nepritel5.delete()
-        score += 100
-    strela.delete()
-    if nepritel1.is_deleted() and nepritel2.is_deleted() and nepritel3.is_deleted() \
-            and nepritel4.is_deleted() and nepritel5.is_deleted():
-        lod.delete()
-        basic.show_string("" + str(level))
-        level = level + 1
-        second_level()
-
-
+        SCORE += 100
+    elif shoot.is_touching(enemy5):
+        enemy5.delete()
+        SCORE += 100
+    shoot.delete()
+    if enemy1.is_deleted() and enemy2.is_deleted() and enemy3.is_deleted() \
+            and enemy4.is_deleted() and enemy5.is_deleted():
+        ship.delete()
+        LEVEL += 1
+        SPEED += 0.5
+        basic.show_string("" + str(LEVEL))
+        first_level()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 
 def on_button_pressed_b():
-    lod.move(1)
-
-
+    ship.move(1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-score = 0
-enemy_strela: game.LedSprite = None
+SCORE = 0
 hitbox2: game.LedSprite = None
 hitbox: game.LedSprite = None
-strela: game.LedSprite = None
-nepritel5: game.LedSprite = None
-nepritel4: game.LedSprite = None
-nepritel3: game.LedSprite = None
-nepritel2: game.LedSprite = None
-nepritel1: game.LedSprite = None
-lod: game.LedSprite = None
-enemy_strela2 = None
-level = 1
-lod = game.create_sprite(2, 4)
+shoot: game.LedSprite = None
+LEVEL = 0
+nepritel: List[number] = []
+nepritel6 = None
+enemy_shoot: game.LedSprite = None
+enemy_shoot2 = None
+music.start_meshipy(music.built_in_meshipy(Meshipies.RINGTONE), MeshipyOptions.ONCE)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
+
+basic.pause(100)
+basic.show_icon(IconNames.SMALL_SQUARE)
+basic.pause(100)
+basic.show_icon(IconNames.SQUARE)
+ship = game.create_sprite(2, 4)
 basic.pause(200)
-nepritel1 = game.create_sprite(0, 0)
+enemy1 = game.create_sprite(0, 0)
 basic.pause(200)
-nepritel2 = game.create_sprite(1, 1)
+enemy2 = game.create_sprite(1, 1)
 basic.pause(200)
-nepritel3 = game.create_sprite(2, 0)
+enemy3 = game.create_sprite(2, 0)
 basic.pause(200)
-nepritel4 = game.create_sprite(3, 1)
+enemy4 = game.create_sprite(3, 1)
 basic.pause(200)
-nepritel5 = game.create_sprite(4, 0)
+enemy5 = game.create_sprite(4, 0)
 TIME = 500
-SPEED = 0.5
+SPEED = 1
 
 
 def on_forever():
-    global enemy_strela
-    enemy_strela = game.create_sprite(randint(0, 4), 0)
-    enemy_strela.change(LedSpriteProperty.BRIGHTNESS, 150)
-    basic.pause(100)
-    enemy_strela.turn(Direction.RIGHT, 90)
-    for index2 in range(4):
-        enemy_strela.move(1)
-        basic.pause(TIME//SPEED)
-    if enemy_strela.is_touching(lod):
-        basic.show_string("GAME OVER!")
-        basic.show_string(str(score))
-    enemy_strela.delete()
-
-
+    global enemy_shoot, LEVEL, SCORE
+    if LEVEL != 0:
+        enemy_shoot = game.create_sprite(randint(0, 4), 0)
+        enemy_shoot.change(LedSpriteProperty.BRIGHTNESS, 150)
+        basic.pause(100)
+        enemy_shoot.turn(Direction.RIGHT, 90)
+        for index2 in range(4):
+            enemy_shoot.move(1)
+            basic.pause(Math.idiv(TIME, SPEED))
+        if enemy_shoot.is_touching(ship):
+            basic.clear_screen()
+            basic.show_string("GAME OVER! " + str(SCORE))
+            basic.clear_screen()
+            SCORE = 0
+            LEVEL = 0
+        enemy_shoot.delete()
 basic.forever(on_forever)
